f829e53af97970b591fa854dceda1eda86e5ecdd
David M. Chen <chen7david@me.com>
2023-07-13 16:59:45 -0300

    added s3 bucked and made it public

--- lib/ts-basic-cdk-examples-stack.ts
+++ lib/ts-basic-cdk-examples-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  aws_s3 as s3,
+  aws_s3_deployment as s3Deploy
+} from 'aws-cdk-lib'
 
 export class TsBasicCdkExamplesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create s3 bucket
+    const bucket = new s3.Bucket(this, 'CDKStaticHostingBucket', {
+      bucketName: 'cdk-static-hosting-bucket',
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsBasicCdkExamplesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // upload static website
+    new s3Deploy.BucketDeployment(this, 'StaticHostingDeploy', {
+      sources: [s3Deploy.Source.asset('./dist')],
+      destinationBucket: bucket
+    })
   }
 }
